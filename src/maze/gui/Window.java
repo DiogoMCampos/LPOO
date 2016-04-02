0f178b2225232a562068942b488a93f8f62d0ca3
DiogoMCampos <d.diogocampos@gmail.com>
2016-04-02 22:12:25 +0100

    Gui package restructured

--- src/maze/gui/Window.java
+++ src/maze/gui/Window.java
@@ -29,24 +29,34 @@ import java.awt.event.ActionListener;
 public class Window {
 
 	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JRootPane rootPane;
+
+	private JLabel lblMazeSize;
+	private JTextField txtMazeSize;
+	private JLabel lblNumberOfDragons;
+	private JTextField txtNumberOfDragons;
+	private JCheckBox randomDragons;
+	private JLabel lblDragonMode;
+	private JComboBox<String> cbDragonMode;
+	private JTextArea mazeArea;
+	private JLabel status;
+
+	private JButton btnNewMaze;
+	private JButton btnExitGame;
 	private JButton btnUp;
 	private JButton btnLeft;
 	private JButton btnRight;
 	private JButton btnDown;
-	private JLabel status;
-	private JComboBox<String> comboBox;
-	private JRootPane rootPane;
 	private KeyStroke upKeyStroke;
 	private KeyStroke downKeyStroke;
 	private KeyStroke rightKeyStroke;
 	private KeyStroke leftKeyStroke;
+
 	private Maze maze;
 	private GraphicInterface gi = new GraphicInterface();
 	private int mazeSize = 11;
 	private int numberDragons = 1;
-	
+
 
 	/**
 	 * Launch the application.
@@ -82,269 +92,21 @@ public class Window {
 		frame.getContentPane().setLayout(null);
 
 		initializeRootPane();
+		initializeMazeOptions();
+		initializeButtons();
 		
-		JLabel lblMazeSize = new JLabel("Maze Size");
-		lblMazeSize.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblMazeSize.setBounds(54, 35, 67, 26);
-		frame.getContentPane().add(lblMazeSize);
+		mazeArea = new JTextArea();
+		mazeArea.setFont(new Font("Courier New", Font.PLAIN, 13));
+		mazeArea.setEditable(false);
+		mazeArea.setBounds(54, 168, 260, 260);
+		frame.getContentPane().add(mazeArea);
 
-		textField = new JTextField();
-		textField.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		textField.setText("11");
-		textField.setBounds(191, 38, 86, 20);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
-
-		JLabel lblNumberOfDragons = new JLabel("Number of Dragons");
-		lblNumberOfDragons.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNumberOfDragons.setBounds(54, 83, 127, 26);
-		frame.getContentPane().add(lblNumberOfDragons);
-
-		textField_1 = new JTextField();
-		textField_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		textField_1.setText("1");
-		textField_1.setBounds(191, 86, 86, 20);
-		frame.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
-
-		JCheckBox checkBox = new JCheckBox("Random");
-		checkBox.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		checkBox.setBounds(290, 87, 97, 23);
-		checkBox.addChangeListener(new ChangeListener()
-		{
-			public void stateChanged(ChangeEvent changeEvent) 
-			{
-				if(checkBox.isSelected())
-					textField_1.setEnabled(false);
-				else
-					textField_1.setEnabled(true);
-			}
-		});
-		frame.getContentPane().add(checkBox);
-
-		JLabel lblNewLabel = new JLabel("Dragon Mode ");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel.setBounds(54, 131, 97, 26);
-		frame.getContentPane().add(lblNewLabel);
-
-		comboBox = new JComboBox<String>();
-		comboBox.setToolTipText("Select the desired dragon behaviour");
-		comboBox.setMaximumRowCount(3);
-		comboBox.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		comboBox.setBounds(172, 133, 189, 22);
-		comboBox.addItem("No movement");
-		comboBox.addItem("Random movement");
-		comboBox.addItem("Random movement and sleep");
-		frame.getContentPane().add(comboBox);
-
-		JTextArea textArea = new JTextArea();
-		textArea.setFont(new Font("Courier New", Font.PLAIN, 13));
-		textArea.setEditable(false);
-		textArea.setBounds(54, 168, 260, 260);
-		frame.getContentPane().add(textArea);
-
-		
-
-		JButton btnNewButton = new JButton("Exit Game");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) 
-			{
-				System.exit(0);
-			}
-		});
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnNewButton.setBounds(397, 85, 107, 23);
-		frame.getContentPane().add(btnNewButton);
-
-
-		btnUp = new JButton("Up");
-		btnUp.setEnabled(false);
-		btnUp.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) 
-			{
-				maze.updateGame(0);
-				String printable = gi.print(maze.getMaze());
-				textArea.setText(printable);
-				int dragonsAlive = maze.dragonsAlive();
-
-				if(maze.getFinished())
-				{
-					closeInputs();
-					if(dragonsAlive == 0)
-						status.setText("You won the game");
-					else
-						status.setText("You were killed!");
-				}
-				else
-				{
-					if(dragonsAlive == 1)
-						status.setText("Move the hero. " +  dragonsAlive + " dragon to go.");
-					else if(dragonsAlive == 0)
-						status.setText("Move to the exit. ");
-					else
-						status.setText("Move the hero. " +  dragonsAlive + " dragons to go.");
-				}
-			}
-		});
-		btnUp.setBounds(395, 254, 70, 23);
-		frame.getContentPane().add(btnUp);
-
-		btnLeft = new JButton("Left");
-		btnLeft.setEnabled(false);
-		btnLeft.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) 
-			{
-				maze.updateGame(1);
-				String printable = gi.print(maze.getMaze());
-				textArea.setText(printable);
-				int dragonsAlive = maze.dragonsAlive();
-
-				if(maze.getFinished())
-				{
-					closeInputs();
-					if(dragonsAlive == 0)
-						status.setText("You won the game");
-					else
-						status.setText("You were killed!");
-				}
-				else
-				{
-
-					if(dragonsAlive == 1)
-						status.setText("Move the hero. " +  dragonsAlive + " dragon to go.");
-					else if(dragonsAlive == 0)
-						status.setText("Move to the exit. ");
-					else
-						status.setText("Move the hero. " +  dragonsAlive + " dragons to go.");
-				}
-			}
-		});
-		btnLeft.setBounds(358, 288, 70, 23);
-		frame.getContentPane().add(btnLeft);
-
-		btnRight = new JButton("Right");
-		btnRight.setEnabled(false);
-		btnRight.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) 
-			{
-				maze.updateGame(3);
-				String printable = gi.print(maze.getMaze());
-				textArea.setText(printable);
-				int dragonsAlive = maze.dragonsAlive();
-				if(maze.getFinished())
-				{
-					closeInputs();
-					if(dragonsAlive == 0)
-						status.setText("You won the game");
-					else
-						status.setText("You were killed!");
-				}
-				else
-				{
-
-					if(dragonsAlive == 1)
-						status.setText("Move the hero. " +  dragonsAlive + " dragon to go.");
-					else if(dragonsAlive == 0)
-						status.setText("Move to the exit. ");
-					else
-						status.setText("Move the hero. " +  dragonsAlive + " dragons to go.");
-				}
-			}
-		});
-		btnRight.setBounds(439, 288, 70, 23);
-		frame.getContentPane().add(btnRight);
-
-		btnDown = new JButton("Down");
-		btnDown.setEnabled(false);
-		btnDown.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) 
-			{
-				maze.updateGame(2);
-				String printable = gi.print(maze.getMaze());
-				textArea.setText(printable);
-
-				int dragonsAlive = maze.dragonsAlive();
-				if(maze.getFinished())
-				{
-					closeInputs();
-					if(dragonsAlive == 0)
-						status.setText("You won the game");
-					else
-						status.setText("You were killed!");
-				}
-				else
-				{
-
-					if(dragonsAlive == 1)
-						status.setText("Move the hero. " +  dragonsAlive + " dragon to go.");
-					else if(dragonsAlive == 0)
-						status.setText("Move to the exit. ");
-					else
-						status.setText("Move the hero. " +  dragonsAlive + " dragons to go.");
-				}
-			}
-		});
-		btnDown.setBounds(395, 322, 70, 23);
-		frame.getContentPane().add(btnDown);
-
-		
-
+		// Initializes the status label
 		status = new JLabel("Create a new maze to play.");
 		status.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		status.setBounds(54, 432, 260, 25);
 		frame.getContentPane().add(status);
-
-		JButton btnNewMaze = new JButton("New Maze");
-		btnNewMaze.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) 
-			{
-				String inputSize = textField.getText();
-				mazeSize = Integer.parseInt(inputSize);
-				if(mazeSize <= 5) // dimension must be higher than five
-				{
-					mazeSize = 7;
-					textField.setText("7");
-				}
-				else if(mazeSize % 2 == 0) // dimension must be odd
-				{
-					mazeSize++;
-					Integer mS = mazeSize;
-					textField.setText(mS.toString());
-				}
-
-				String inputDragons = textField_1.getText();
-				numberDragons = Integer.parseInt(inputDragons);
-				if(numberDragons < 0 || checkBox.isSelected())
-				{
-					numberDragons = 0;	
-				}
-
-				textArea.setEditable(true);
-				btnUp.setEnabled(true);
-				btnLeft.setEnabled(true);
-				btnRight.setEnabled(true);
-				btnDown.setEnabled(true);
-
-				MazeBuilder mb = new MazeBuilder(mazeSize, numberDragons);
-				char [][] matrix = mb.getMaze();
-				maze = new Maze(matrix);
-
-				status.setText("Move the hero to pick the sword.");
-
-				int mode = comboBox.getSelectedIndex();
-				maze.setMode(mode);
-				System.out.println(mode);
-
-				String printable = gi.print(matrix);
-				textArea.setText(printable);
-			}
-		});
-		btnNewMaze.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnNewMaze.setBounds(397, 38, 107, 23);
-		frame.getContentPane().add(btnNewMaze);
-
 	}
-
 
 	public void closeInputs()
 	{
@@ -353,11 +115,11 @@ public class Window {
 		btnUp.setEnabled(false);
 		btnDown.setEnabled(false);
 	}
-	
+
 	public void initializeRootPane()
 	{
 		rootPane = frame.getRootPane();
-		
+
 		Action upKeyAction = new AbstractAction("upKeyAction") {
 
 			@Override
@@ -394,7 +156,7 @@ public class Window {
 		downKeyStroke = KeyStroke.getKeyStroke("DOWN");
 		rightKeyStroke = KeyStroke.getKeyStroke("RIGHT");
 		leftKeyStroke = KeyStroke.getKeyStroke("LEFT");
-		
+
 		rootPane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(upKeyStroke, "upKeyAction");
 		rootPane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(downKeyStroke, "downKeyAction");
 		rootPane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(rightKeyStroke, "rightKeyAction");
@@ -405,4 +167,210 @@ public class Window {
 		rootPane.getActionMap().put("leftKeyAction", leftKeyAction);
 	}
 
+	public void initializeMazeOptions() {
+		// Initializes the label and field related to the Maze Size
+		lblMazeSize = new JLabel("Maze Size");
+		lblMazeSize.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblMazeSize.setBounds(54, 35, 67, 26);
+		frame.getContentPane().add(lblMazeSize);
+
+		txtMazeSize = new JTextField();
+		txtMazeSize.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		txtMazeSize.setText("11");
+		txtMazeSize.setBounds(191, 38, 86, 20);
+		frame.getContentPane().add(txtMazeSize);
+		txtMazeSize.setColumns(10);
+
+		// Initializes the label and field related to the number of Dragons
+		lblNumberOfDragons = new JLabel("Number of Dragons");
+		lblNumberOfDragons.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNumberOfDragons.setBounds(54, 83, 127, 26);
+		frame.getContentPane().add(lblNumberOfDragons);
+
+		txtNumberOfDragons = new JTextField();
+		txtNumberOfDragons.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		txtNumberOfDragons.setText("1");
+		txtNumberOfDragons.setBounds(191, 86, 86, 20);
+		frame.getContentPane().add(txtNumberOfDragons);
+		txtNumberOfDragons.setColumns(10);
+
+		// Initializes the random number of Dragons checkbox
+		randomDragons = new JCheckBox("Random");
+		randomDragons.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		randomDragons.setBounds(290, 87, 97, 23);
+		randomDragons.addChangeListener(new ChangeListener()
+		{
+			public void stateChanged(ChangeEvent changeEvent) 
+			{
+				if(randomDragons.isSelected())
+					txtNumberOfDragons.setEnabled(false);
+				else
+					txtNumberOfDragons.setEnabled(true);
+			}
+		});
+		frame.getContentPane().add(randomDragons);
+
+		// Initializes the label and ComboBox related to the dragon mode
+		lblDragonMode = new JLabel("Dragon Mode ");
+		lblDragonMode.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblDragonMode.setBounds(54, 131, 97, 26);
+		frame.getContentPane().add(lblDragonMode);
+
+		cbDragonMode = new JComboBox<String>();
+		cbDragonMode.setToolTipText("Select the desired dragon behaviour");
+		cbDragonMode.setMaximumRowCount(3);
+		cbDragonMode.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		cbDragonMode.setBounds(172, 133, 189, 22);
+		cbDragonMode.addItem("No movement");
+		cbDragonMode.addItem("Random movement");
+		cbDragonMode.addItem("Random movement and sleep");
+		frame.getContentPane().add(cbDragonMode);
+	}
+	
+	public void initializeButtons() {
+		
+		initializeMovementButtons();
+		
+		btnNewMaze = new JButton("New Maze");
+		btnNewMaze.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				String inputSize = txtMazeSize.getText();
+				mazeSize = Integer.parseInt(inputSize);
+				if(mazeSize <= 5) // dimension must be higher than five
+				{
+					mazeSize = 7;
+					txtMazeSize.setText("7");
+				}
+				else if(mazeSize % 2 == 0) // dimension must be odd
+				{
+					mazeSize++;
+					Integer mS = mazeSize;
+					txtMazeSize.setText(mS.toString());
+				}
+				
+				// since freeSpaces = 7 * mazeSize - 35 and maxDragons = freeSpaces / 7 
+				int maxNumberDragons = mazeSize - 5;
+
+				String inputDragons = txtNumberOfDragons.getText();
+				numberDragons = Integer.parseInt(inputDragons);
+				if(numberDragons < 0 || randomDragons.isSelected())
+				{
+					numberDragons = 0;	
+				}
+				else if (numberDragons > maxNumberDragons)
+				{
+					numberDragons = maxNumberDragons;
+					Integer nD = numberDragons;
+					txtNumberOfDragons.setText(nD.toString());
+				}
+
+				mazeArea.setEditable(true);
+				btnUp.setEnabled(true);
+				btnLeft.setEnabled(true);
+				btnRight.setEnabled(true);
+				btnDown.setEnabled(true);
+
+				MazeBuilder mb = new MazeBuilder(mazeSize, numberDragons);
+				char [][] matrix = mb.getMaze();
+				maze = new Maze(matrix);
+
+				status.setText("Move the hero to pick the sword.");
+
+				int mode = cbDragonMode.getSelectedIndex();
+				maze.setMode(mode);
+				System.out.println(mode);
+
+				String printable = gi.print(matrix);
+				mazeArea.setText(printable);
+			}
+		});
+		btnNewMaze.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnNewMaze.setBounds(397, 38, 107, 23);
+		frame.getContentPane().add(btnNewMaze);
+
+		btnExitGame = new JButton("Exit Game");
+		btnExitGame.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				System.exit(0);
+			}
+		});
+		btnExitGame.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnExitGame.setBounds(397, 85, 107, 23);
+		frame.getContentPane().add(btnExitGame);
+		
+	}
+	
+	public void initializeMovementButtons() {
+		btnUp = new JButton("Up");
+		btnUp.setEnabled(false);
+		btnUp.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				updateMovement(0);
+			}
+		});
+		btnUp.setBounds(395, 254, 70, 23);
+		frame.getContentPane().add(btnUp);
+
+		btnLeft = new JButton("Left");
+		btnLeft.setEnabled(false);
+		btnLeft.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				updateMovement(1);
+			}
+		});
+		btnLeft.setBounds(358, 288, 70, 23);
+		frame.getContentPane().add(btnLeft);
+
+		btnRight = new JButton("Right");
+		btnRight.setEnabled(false);
+		btnRight.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				updateMovement(3);
+			}
+		});
+		btnRight.setBounds(439, 288, 70, 23);
+		frame.getContentPane().add(btnRight);
+
+		btnDown = new JButton("Down");
+		btnDown.setEnabled(false);
+		btnDown.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				updateMovement(2);
+			}
+		});
+		btnDown.setBounds(395, 322, 70, 23);
+		frame.getContentPane().add(btnDown);	
+	}
+	
+	public void updateMovement(int direction) {
+		maze.updateGame(direction);
+		String printable = gi.print(maze.getMaze());
+		mazeArea.setText(printable);
+
+		int dragonsAlive = maze.dragonsAlive();
+		if(maze.getFinished())
+		{
+			closeInputs();
+			if(dragonsAlive == 0)
+				status.setText("You won the game");
+			else
+				status.setText("You were killed!");
+		}
+		else
+		{
+
+			if(dragonsAlive == 1)
+				status.setText("Move the hero. " +  dragonsAlive + " dragon to go.");
+			else if(dragonsAlive == 0)
+				status.setText("Move to the exit. ");
+			else
+				status.setText("Move the hero. " +  dragonsAlive + " dragons to go.");
+		}
+	}
 }
